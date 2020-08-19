9931398029ea2ec474e2ae8fc8381024b294f243
Kashyap Patel <khpatel4991@gmail.com>
2020-08-19 01:42:30 -0400

    Remove unnecessary files

--- fetchApi/getVideo.js
+++
@@ -1,8 +0,0 @@
-const { allVideos } = require("../fixtures/videos");
-
-const fakeDelay = (ms = 1000) => new Promise(res => setTimeout(res, ms));
-
-module.exports = async slug => {
-  await fakeDelay(500);
-  return allVideos.find(video => video.slug === slug) || null;
-};
--- fetchApi/index.js
+++ fetchApi/index.js
@@ -1,13 +1,9 @@
 const getPlayer = require("./getPlayer");
 const getCards = require("./getCards");
 const getClan = require("./getClan");
-const getVideos = require("./getVideos");
-const getVideo = require("./getVideo");
 
 module.exports = {
   getPlayer,
   getClan,
   getCards,
-  getVideo,
-  getVideos
 };
